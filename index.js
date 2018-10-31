06d71d8bbb8a4fe991147c5b10f88c00f1ff0302
alonssro <rodriguezalonsod@outlook.com>
2018-10-30 22:43:37 -0600

    Scale Band

--- index.js
+++ index.js
@@ -4,24 +4,30 @@ const svg = d3.select('svg');
 
 d3.json('menu.json').then(data => {
 
-  const y = d3.scaleLinear(data)
+  const y = d3.scaleLinear()
     .domain([0, 1000])
     .range([0, 500])
+
+  const x = d3.scaleBand()
+    .domain(data.map(item => item.name))
+    .range([0, 600])
+    .paddingInner(0.2)
+    .paddingOuter(0.2);
 
   //join the data to rects
   const rects = svg.selectAll('rect')
     .data(data);
 
   //join attrs to existing rect in the DOM
-  rects.attr('width', 50)
+  rects.attr('width', x.bandwidth)
     .attr('height', d => y(d.orders))
     .attr('fill', 'orange')
-    .attr('x', (d, i) => i * 70);
+    .attr('x', d => x(d.name));
 
   //join the enter selection
   rects.enter()
-    .append('rect').attr('width', 50)
+    .append('rect').attr('width', x.bandwidth)
     .attr('height', d => y(d.orders))
     .attr('fill', 'orange')
-    .attr('x', (d, i) => i * 70);
+    .attr('x', d => x(d.name));
 })
