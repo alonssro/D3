35932ffdce14960d1bffd7fca8fd1c45778109ca
alonssro <rodriguezalonsod@outlook.com>
2018-10-30 22:29:53 -0600

    Linear Scales to Barcharts

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-//select svg container first
+//Select the svg first
 
 const svg = d3.select('svg');
 
-d3.json('planets.json').then(data => {
+d3.json('menu.json').then(data => {
 
-  //join the data to the circs
+  const y = d3.scaleLinear(data)
+    .domain([0, 1000])
+    .range([0, 500])
 
-  const circs = svg
-    .selectAll('cicle')
-    .data(data)
+  //join the data to rects
+  const rects = svg.selectAll('rect')
+    .data(data);
 
-  //add attrs to circs already in the DOM
-  circs
-    .attr('cy', 200)
-    .attr('cx', d => d.distance)
-    .attr('r', d => d.radius)
-    .attr('fill', d => d.fill);
+  //join attrs to existing rect in the DOM
+  rects.attr('width', 50)
+    .attr('height', d => y(d.orders))
+    .attr('fill', 'orange')
+    .attr('x', (d, i) => i * 70);
 
-  //append the enter selection to the DOM
-  circs.enter()
-    .append('circle')
-    .attr('cy', 200)
-    .attr('cx', d => d.distance)
-    .attr('r', d => d.radius)
-    .attr('fill', d => d.fill);
+  //join the enter selection
+  rects.enter()
+    .append('rect').attr('width', 50)
+    .attr('height', d => y(d.orders))
+    .attr('fill', 'orange')
+    .attr('x', (d, i) => i * 70);
 })
