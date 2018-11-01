c2286b4b32da207dd93f215c1ce03720820b3c6b
alonssro <rodriguezalonsod@outlook.com>
2018-10-31 23:33:38 -0600

    Groups & margins

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 //Select the svg first
+const svg = d3.select('.canvas')
+  .append('svg')
+  .attr('width', 600)
+  .attr('height', 600);
 
-const svg = d3.select('svg');
+//create some margins and dimensions
+const margin = {
+  top: 20,
+  right: 20,
+  bottom: 100,
+  left: 100
+}
+
+const graphWidth = 600 - margin.left - margin.right;
+const graphHeight = 600 - margin.top - margin.bottom;
+
+const graph = svg.append('g')
+  .attr('width', graphWidth)
+  .attr('height', graphHeight)
+  .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+const xAxisGroup = graph.append('g')
+  .attr("transform", `translate(0, ${graphHeight})`);
+const yAxisGroup = graph.append('g');
 
 d3.json('menu.json').then(data => {
 
   const y = d3.scaleLinear()
     .domain([0, d3.max(data, d => d.orders)])
-    .range([0, 500])
+    .range([0, graphHeight])
 
   /*   const min = d3.min(data, d => d.orders)
     const max = d3.max(data, d => d.orders)
     const extent = d3.extent(data, d => d.orders) */
-
 
   const x = d3.scaleBand()
     .domain(data.map(item => item.name))
@@ -20,7 +41,7 @@ d3.json('menu.json').then(data => {
     .paddingOuter(0.2);
 
   //join the data to rects
-  const rects = svg.selectAll('rect')
+  const rects = graph.selectAll('rect')
     .data(data);
 
   //join attrs to existing rect in the DOM
@@ -31,8 +52,17 @@ d3.json('menu.json').then(data => {
 
   //join the enter selection
   rects.enter()
-    .append('rect').attr('width', x.bandwidth)
+    .append('rect')
+    .attr('width', x.bandwidth)
     .attr('height', d => y(d.orders))
     .attr('fill', 'orange')
     .attr('x', d => x(d.name));
+
+
+  //create and call the axes
+  const xAxis = d3.axisBottom(x);
+  const yAxis = d3.axisLeft(y);
+
+  xAxisGroup.call(xAxis);
+  yAxisGroup.call(yAxis);
 })
