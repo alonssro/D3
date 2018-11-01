847cf4fb697cc47c90909a16da3fb70189a1ec8f
alonssro <rodriguezalonsod@outlook.com>
2018-10-31 23:49:55 -0600

    Inverting graph

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 //Select the svg first
-const svg = d3.select('.canvas')
-  .append('svg')
-  .attr('width', 600)
-  .attr('height', 600);
+const svg = d3
+  .select(".canvas")
+  .append("svg")
+  .attr("width", 800)
+  .attr("height", 600);
 
 //create some margins and dimensions
 const margin = {
@@ -10,54 +11,59 @@ const margin = {
   right: 20,
   bottom: 100,
   left: 100
-}
+};
 
 const graphWidth = 600 - margin.left - margin.right;
 const graphHeight = 600 - margin.top - margin.bottom;
 
-const graph = svg.append('g')
-  .attr('width', graphWidth)
-  .attr('height', graphHeight)
-  .attr('transform', `translate(${margin.left}, ${margin.top})`)
+const graph = svg
+  .append("g")
+  .attr("width", graphWidth)
+  .attr("height", graphHeight)
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-const xAxisGroup = graph.append('g')
+const xAxisGroup = graph
+  .append("g")
   .attr("transform", `translate(0, ${graphHeight})`);
-const yAxisGroup = graph.append('g');
+const yAxisGroup = graph.append("g");
 
-d3.json('menu.json').then(data => {
-
-  const y = d3.scaleLinear()
+d3.json("menu.json").then(data => {
+  const y = d3
+    .scaleLinear()
     .domain([0, d3.max(data, d => d.orders)])
-    .range([0, graphHeight])
+    .range([graphHeight, 0]);
 
   /*   const min = d3.min(data, d => d.orders)
     const max = d3.max(data, d => d.orders)
     const extent = d3.extent(data, d => d.orders) */
 
-  const x = d3.scaleBand()
+  const x = d3
+    .scaleBand()
     .domain(data.map(item => item.name))
     .range([0, 600])
     .paddingInner(0.2)
     .paddingOuter(0.2);
 
   //join the data to rects
-  const rects = graph.selectAll('rect')
-    .data(data);
+  const rects = graph.selectAll("rect").data(data);
 
   //join attrs to existing rect in the DOM
-  rects.attr('width', x.bandwidth)
-    .attr('height', d => y(d.orders))
-    .attr('fill', 'orange')
-    .attr('x', d => x(d.name));
+  rects
+    .attr("width", x.bandwidth)
+    .attr("height", d => graphHeight - y(d.orders))
+    .attr("fill", "orange")
+    .attr("x", d => x(d.name))
+    .attr("y", d => y(d.orders));
 
   //join the enter selection
-  rects.enter()
-    .append('rect')
-    .attr('width', x.bandwidth)
-    .attr('height', d => y(d.orders))
-    .attr('fill', 'orange')
-    .attr('x', d => x(d.name));
-
+  rects
+    .enter()
+    .append("rect")
+    .attr("width", x.bandwidth)
+    .attr("height", d => graphHeight - y(d.orders))
+    .attr("fill", "orange")
+    .attr("x", d => x(d.name))
+    .attr("y", d => y(d.orders));
 
   //create and call the axes
   const xAxis = d3.axisBottom(x);
@@ -65,4 +71,4 @@ d3.json('menu.json').then(data => {
 
   xAxisGroup.call(xAxis);
   yAxisGroup.call(yAxis);
-})
+});
