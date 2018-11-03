034984bd9744431fa4c8966e3f5f5d3ffd2c6a6b
alonssro <rodriguezalonsod@outlook.com>
2018-11-03 13:38:31 -0600

    Realtime data in Firestore

--- index.js
+++ index.js
@@ -76,14 +76,30 @@ const update = (data) => {
 
 };
 
-db.collection('dishes').get().then(res => {
+data = [];
 
-  var data = [];
-  res.docs.forEach(doc => {
-    data.push(doc.data());
-  });
+db.collection('dishes').onSnapshot(res => {
 
-  d3.interval(() => {
-    update(data);
-  }, 1000)
-});
+  res.docChanges().forEach(change => {
+
+    const doc = {
+      ...change.doc.data(),
+      id: change.doc.id
+    }
+    switch (change.type) {
+      case 'added':
+        data.push(doc)
+        break;
+      case 'modified':
+        const index = data.findIndex(item => item.id == doc.id);
+        data[index] = doc
+        break;
+      case 'removed':
+        data = data.filter(item => item.id != doc.id);
+      default:
+        break;
+    }
+  })
+  update(data);
+
+})
